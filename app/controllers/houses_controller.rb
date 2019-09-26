fa3cafbb7d9d0b23798d862205db0ff83477c8dc
Kyle Geske <stungeye@gmail.com>
2019-09-26 17:32:26 -0500

    Implemented the houses index action and associated view.

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class HousesController < ApplicationController
   def index
+    @houses = House.all
   end
+  # Automagically load up the associated view.
+  # app/views/houses/index.html.erb
+  # All instance variables are shared with the view.
 
-  def show
-  end
+  def show; end
 end
