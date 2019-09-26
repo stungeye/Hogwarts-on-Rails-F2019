06836828ae134e170adc2f9ea3c29f39d0b8d7c2
Kyle Geske <stungeye@gmail.com>
2019-09-26 16:56:56 -0500

    Added the house to student associations.

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class House < ApplicationRecord
+  has_many :students
+
   validates :name, :points, presence: true
   validates :points, numericality: { only_integer: true }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Student < ApplicationRecord
   belongs_to :house
+
+  validates :name, presense: true
 end
