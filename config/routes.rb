b03dde81ec56f3a5e40dbbecc6d1dc07e2627e83
Kyle Geske <stungeye@gmail.com>
2019-09-27 08:30:37 -0500

    Links to and from the show and index houses views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/houses', to: 'houses#index'
-  get '/houses/:id', to: 'houses#show', id: /\d+/
+  resources :houses, only: %i[index show]
+  # ^ This command creates the same routes as the following two:
+  # get '/houses', to: 'houses#index'
+  # get '/houses/:id', to: 'houses#show', id: /\d+/
 
   root to: 'houses#index'
 end
