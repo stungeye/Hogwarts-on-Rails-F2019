b1b3124df177a947cee65dbdde1bafbf8ea0cd9f
Kyle Geske <stungeye@gmail.com>
2019-10-03 16:25:04 -0500

    Added a scaffold for a Page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :pages
   resources :appointments, only: :index
   # get /appointments, to: 'appointments#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_203220) do
+ActiveRecord::Schema.define(version: 2019_10_03_212442) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "scheduled"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2019_10_02_203220) do
   create_table "houses", force: :cascade do |t|
     t.string "name"
     t.integer "points"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
